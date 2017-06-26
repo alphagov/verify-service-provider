62c2fe2035c75dec288590257b0b9a14715d5558
Hristo Georgiev <hristo.georgiev@digital.cabinet-office.gov.uk>
2017-06-26 16:48:48 +0100

    TT:-642: Fixed test after hardcoded pit changes.
    Authors: @georgievh

--- src/test/java/uk/gov/ida/verifyserviceprovider/resources/TranslateSamlResponseResourceTest.java
+++ src/test/java/uk/gov/ida/verifyserviceprovider/resources/TranslateSamlResponseResourceTest.java
@@ -5,77 +5,90 @@ import io.dropwizard.testing.junit.ResourceTestRule;
 import org.json.JSONObject;
 import org.junit.ClassRule;
 import org.junit.Test;
-import uk.gov.ida.verifyserviceprovider.dto.LevelOfAssurance;
 import uk.gov.ida.verifyserviceprovider.dto.TranslateSamlResponseBody;
 import uk.gov.ida.verifyserviceprovider.dto.TranslatedResponseBody;
 
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 import java.util.Base64;
 import java.util.Map;
 
+import static javax.ws.rs.client.Entity.entity;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.ida.verifyserviceprovider.dto.LevelOfAssurance.LEVEL_1;
+import static uk.gov.ida.verifyserviceprovider.dto.LevelOfAssurance.LEVEL_2;
 
 public class TranslateSamlResponseResourceTest {
 
     @ClassRule
     public static final ResourceTestRule resources = ResourceTestRule.builder()
-            .addResource(new TranslateSamlResponseResource())
-            .build();
+        .addResource(new TranslateSamlResponseResource())
+        .build();
 
     @Test
     public void translateAuthnResponseWithSuccessfulLOA1Match() {
+        Map<String, String> data = ImmutableMap.of(
+            "levelOfAssurance", "LEVEL_1",
+            "pid", "some-pid-id"
+        );
+
         TranslateSamlResponseBody translateSamlResponseBody = new TranslateSamlResponseBody(
-            getSamlResponseForLOA("LEVEL_1"),
+            getSamlResponseFor(data),
             "secure-token"
         );
 
-        Response response = resources.target("/translate-response").request().post(Entity.entity(translateSamlResponseBody, MediaType.APPLICATION_JSON_TYPE));
-        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
-
+        Response response = resources.target("/translate-response").request().post(entity(translateSamlResponseBody, APPLICATION_JSON_TYPE));
         TranslatedResponseBody translatedResponseBody = response.readEntity(TranslatedResponseBody.class);
-        assertThat(translatedResponseBody.pid).isNotEmpty();
-        assertThat(translatedResponseBody.levelOfAssurance).isEqualTo(LevelOfAssurance.LEVEL_1);
+
+        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+        assertThat(translatedResponseBody.pid).isEqualTo("some-pid-id");
+        assertThat(translatedResponseBody.levelOfAssurance).isEqualTo(LEVEL_1);
         assertThat(translatedResponseBody.attributes).isEmpty();
     }
 
     @Test
     public void translateAuthnResponseWithSuccessfulLOA2Match() {
+        Map<String, String> data = ImmutableMap.of(
+            "levelOfAssurance", "LEVEL_2",
+            "pid", "some-pid-id"
+        );
+
         TranslateSamlResponseBody translateSamlResponseBody = new TranslateSamlResponseBody(
-            getSamlResponseForLOA("LEVEL_2"),
+            getSamlResponseFor(data),
             "secure-token"
         );
 
-        Response response = resources.target("/translate-response").request().post(Entity.entity(translateSamlResponseBody, MediaType.APPLICATION_JSON_TYPE));
-        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
-
+        Response response = resources.target("/translate-response").request().post(entity(translateSamlResponseBody, APPLICATION_JSON_TYPE));
         TranslatedResponseBody translatedResponseBody = response.readEntity(TranslatedResponseBody.class);
-        assertThat(translatedResponseBody.pid).isNotEmpty();
-        assertThat(translatedResponseBody.levelOfAssurance).isEqualTo(LevelOfAssurance.LEVEL_2);
+
+        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+        assertThat(translatedResponseBody.pid).isEqualTo("some-pid-id");
+        assertThat(translatedResponseBody.levelOfAssurance).isEqualTo(LEVEL_2);
         assertThat(translatedResponseBody.attributes).isEmpty();
     }
 
     @Test
     public void translateAuthnResponseShouldReturn400WhenUnknownLevel() {
+        Map<String, String> data = ImmutableMap.of(
+            "levelOfAssurance", "some-unknown-level",
+            "pid", "some-pid-id"
+        );
+
         TranslateSamlResponseBody translateSamlResponseBody = new TranslateSamlResponseBody(
-            getSamlResponseForLOA("some-unknown-level"),
+            getSamlResponseFor(data),
             "secure-token"
         );
 
-        Response response = resources.target("/translate-response").request().post(Entity.entity(translateSamlResponseBody, MediaType.APPLICATION_JSON_TYPE));
+        Response response = resources.target("/translate-response")
+            .request()
+            .post(entity(translateSamlResponseBody, APPLICATION_JSON_TYPE));
 
-        assertThat(response.getStatus()).isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
+        assertThat(response.getStatus()).isEqualTo(BAD_REQUEST.getStatusCode());
     }
 
-    private String getSamlResponseForLOA(String levelOfAssurance) {
-        Map<String, String> samlResponseData = ImmutableMap.of(
-            "levelOfAssurance", levelOfAssurance,
-            "pid", "some-pid-value"
-        );
-
-        String samlRequestJson = new JSONObject(samlResponseData).toString();
+    private String getSamlResponseFor(Map<String, String> data) {
+        String samlRequestJson = new JSONObject(data).toString();
         return new String(Base64.getEncoder().encode(samlRequestJson.getBytes()));
     }
 }
