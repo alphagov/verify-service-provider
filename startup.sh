56cb3153eaa379c895b1b04a175ad9548b0c0a8b
Tuomas Nylund <tunylund@gmail.com>
2017-07-13 10:35:44 +0100

    TT-725: make startup script work with versioned distributions
    The startup script used to use distZip to build the application
    distribution and then extract it and run it. As the archive would
    contain a changing version number, it would be gnarly to accomodate
    the version number in this script.
    
    All of the above can be skipped by using the installDist task
    instead.
    
    Author: @tunylund

--- startup.sh
+++ startup.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 # something about using gradles distZip to:
-./gradlew distZip
+./gradlew installDist 
 
-tar -xf build/distributions/verify-service-provider.zip -C build/distributions/
-
-./build/distributions/verify-service-provider/bin/verify-service-provider server ./configuration/verify-service-provider.yml
+./build/install/verify-service-provider/bin/verify-service-provider server ./configuration/verify-service-provider.yml
