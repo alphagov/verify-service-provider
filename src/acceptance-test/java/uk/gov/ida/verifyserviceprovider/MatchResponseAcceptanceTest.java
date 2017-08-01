63f682f680f2ee66c4406e688107f7c809e864d3
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2017-08-01 17:15:58 +0100

    TT-858: Acceptance test for basic match response
    Added example xml files of saml response and of an unencrypted assertion
    
    Authors: @tunylund @andy-paine @richardtowers @IreneLau-GDS @georgievh
    @sgreensmith

---
+++ src/acceptance-test/java/uk/gov/ida/verifyserviceprovider/MatchResponseAcceptanceTest.java
@@ -0,0 +1,111 @@
+package uk.gov.ida.verifyserviceprovider;
+
+import com.google.common.collect.ImmutableMap;
+import io.dropwizard.testing.ConfigOverride;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import uk.gov.ida.verifyserviceprovider.configuration.VerifyServiceProviderConfiguration;
+import uk.gov.ida.verifyserviceprovider.dto.LevelOfAssurance;
+import uk.gov.ida.verifyserviceprovider.dto.RequestGenerationBody;
+import uk.gov.ida.verifyserviceprovider.dto.RequestResponseBody;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.dropwizard.testing.ResourceHelpers.resourceFilePath;
+import static javax.ws.rs.core.Response.Status.OK;
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.ida.saml.core.test.TestCertificateStrings.TEST_RP_PRIVATE_SIGNING_KEY;
+import static uk.gov.ida.saml.core.test.TestCertificateStrings.TEST_RP_PUBLIC_SIGNING_CERT;
+
+public class MatchResponseAcceptanceTest {
+
+    private static String COMPLIANCE_TOOL_HOST = "https://compliance-tool-reference.ida.digital.cabinet-office.gov.uk";
+
+    @ClassRule
+    public static DropwizardAppRule<VerifyServiceProviderConfiguration> application = new DropwizardAppRule<>(
+        VerifyServiceProviderApplication.class,
+        resourceFilePath("verify-service-provider-acceptance-test.yml"),
+        ConfigOverride.config("samlSigningKey", TEST_RP_PRIVATE_SIGNING_KEY),
+        ConfigOverride.config("hubSsoLocation", String.format("%s/%s", COMPLIANCE_TOOL_HOST, "SAML2/SSO"))
+    );
+
+    private static Client client = application.client();
+    private String successCaseURI;
+
+    @Before
+    public void setupComplianceTool() throws Exception {
+        URL testDataURL = MatchResponseAcceptanceTest.class.getResource("/compliance-tool-service-test-data.json");
+
+        JSONObject serviceTestData = new JSONObject(new String(Files.readAllBytes(Paths.get(testDataURL.toURI()))));
+        serviceTestData.put("signingCertificate", TEST_RP_PUBLIC_SIGNING_CERT);
+
+        Response complianceToolResponse = client
+            .target(URI.create(String.format("%s/%s", COMPLIANCE_TOOL_HOST, "service-test-data")))
+            .request()
+            .buildPost(Entity.json(serviceTestData.toString()))
+            .invoke();
+
+        assertThat(complianceToolResponse.getStatus()).isEqualTo(OK.getStatusCode());
+
+        Response authnResponse = client
+            .target(URI.create(String.format("http://localhost:%d/generate-request", application.getLocalPort())))
+            .request()
+            .buildPost(Entity.json(new RequestGenerationBody(LevelOfAssurance.LEVEL_2)))
+            .invoke();
+
+        RequestResponseBody authnSaml = authnResponse.readEntity(RequestResponseBody.class);
+
+        Response complianceToolSsoResponse = client
+            .target(authnSaml.getSsoLocation())
+            .request()
+            .buildPost(Entity.form(new MultivaluedHashMap<>(ImmutableMap.of("SAMLRequest", authnSaml.getSamlRequest()))))
+            .invoke();
+
+        JSONObject complianceToolResponseBody = new JSONObject(complianceToolSsoResponse.readEntity(String.class));
+        String responseGeneratorLocation = complianceToolResponseBody.getString("responseGeneratorLocation");
+
+        Response complianceToolScenariosResponse = client.target(responseGeneratorLocation)
+            .request()
+            .buildGet()
+            .invoke();
+
+        JSONObject complianceToolScenarios = new JSONObject(complianceToolScenariosResponse.readEntity(String.class));
+        JSONArray testCases = complianceToolScenarios.getJSONArray("testCases");
+        JSONObject successCase = testCases.getJSONObject(0);
+
+        successCaseURI = successCase.getString("executeUri");
+    }
+
+    private String extractSamlResponse(String complianceToolResponseBody) {
+        Elements responseElements = Jsoup.parse(complianceToolResponseBody)
+            .getElementsByAttributeValue("name", "SAMLResponse");
+        return responseElements.get(0).val();
+    }
+
+    @Test
+    public void shouldFoo() {
+
+        Response response = client.target(successCaseURI)
+            .request()
+            .buildGet()
+            .invoke();
+        String responseHtmlString = response.readEntity(String.class);
+        String samlString = extractSamlResponse(responseHtmlString);
+
+        assertThat(samlString).isNotEmpty();
+    }
+}
