51c629b63884debdb30dd06b25f1bca60296104c
IreneLau-GDS <irene.lau@digital.cabinet-office.gov.uk>
2017-08-08 10:17:16 +0100

    TT-858 Change error message content assertion
    The errors can be ordered differently so can't do a straight
    string compare.
    
    Authors: @IreneLau-GDS @richardtowers @georgievh

--- src/test/java/unit/uk/gov/ida/verifyserviceprovider/resources/TranslateSamlResponseResourceTest.java
+++ src/test/java/unit/uk/gov/ida/verifyserviceprovider/resources/TranslateSamlResponseResourceTest.java
@@ -1,5 +1,6 @@
 package unit.uk.gov.ida.verifyserviceprovider.resources;
 
+import com.google.common.collect.ImmutableSet;
 import io.dropwizard.testing.junit.ResourceTestRule;
 import org.apache.http.HttpStatus;
 import org.json.JSONObject;
@@ -14,6 +15,9 @@ import uk.gov.ida.verifyserviceprovider.resources.TranslateSamlResponseResource;
 import uk.gov.ida.verifyserviceprovider.services.ResponseService;
 
 import javax.ws.rs.core.Response;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static javax.ws.rs.client.Entity.json;
 import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
@@ -75,9 +79,12 @@ public class TranslateSamlResponseResourceTest {
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.SC_UNPROCESSABLE_ENTITY);
 
-        ErrorBody actualError = response.readEntity(ErrorBody.class);
-        assertThat(actualError.getReason()).isEqualTo(String.valueOf(HttpStatus.SC_UNPROCESSABLE_ENTITY));
-        assertThat(actualError.getMessage()).isEqualTo("requestId may not be null, samlResponse may not be null");
+        ErrorBody actualErrorBody = response.readEntity(ErrorBody.class);
+        assertThat(actualErrorBody.getReason()).isEqualTo(String.valueOf(HttpStatus.SC_UNPROCESSABLE_ENTITY));
+
+        Set<String> expectedErrors = ImmutableSet.of("requestId may not be null", "samlResponse may not be null");
+        Set<String> actualErrors = Arrays.stream(actualErrorBody.getMessage().split(", ")).collect(Collectors.toSet());
+        assertThat(actualErrors).isEqualTo(expectedErrors);
     }
 
 }
