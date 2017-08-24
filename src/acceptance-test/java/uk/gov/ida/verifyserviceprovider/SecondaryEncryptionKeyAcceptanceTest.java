a939c3ad6bd22127b0fb93637b0db577473d5d39
Andy Paine <andy.paine@outlook.com>
2017-08-24 13:32:01 +0100

    BAU: Fix logging verbosity
    Removed debug log level from all non-test config files
    Added Scenario to translate-response logging
    Optimised imports across project
    
    Authors: @andy-paine @rachaelbooth

--- src/acceptance-test/java/uk/gov/ida/verifyserviceprovider/SecondaryEncryptionKeyAcceptanceTest.java
+++ src/acceptance-test/java/uk/gov/ida/verifyserviceprovider/SecondaryEncryptionKeyAcceptanceTest.java
@@ -2,38 +2,27 @@ package uk.gov.ida.verifyserviceprovider;
 
 import com.google.common.collect.ImmutableMap;
 import common.uk.gov.ida.verifyserviceprovider.servers.MockMsaServer;
-import io.dropwizard.jersey.errors.ErrorMessage;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
-import sun.security.rsa.RSAKeyPairGenerator;
 import uk.gov.ida.verifyserviceprovider.dto.RequestResponseBody;
 import uk.gov.ida.verifyserviceprovider.dto.TranslatedResponseBody;
 import uk.gov.ida.verifyserviceprovider.rules.VerifyServiceProviderAppRule;
 import uk.gov.ida.verifyserviceprovider.services.ComplianceToolService;
 import uk.gov.ida.verifyserviceprovider.services.GenerateRequestService;
-import uk.gov.ida.verifyserviceprovider.utils.Crypto;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.core.Response;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.util.Map;
 
 import static javax.ws.rs.client.Entity.json;
-import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 import static javax.ws.rs.core.Response.Status.OK;
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.ida.saml.core.test.TestCertificateStrings.TEST_RP_MS_PRIVATE_ENCRYPTION_KEY;
-import static uk.gov.ida.saml.core.test.TestCertificateStrings.TEST_RP_MS_PRIVATE_SIGNING_KEY;
 import static uk.gov.ida.saml.core.test.TestCertificateStrings.TEST_RP_MS_PUBLIC_ENCRYPTION_CERT;
-import static uk.gov.ida.saml.core.test.TestCertificateStrings.TEST_RP_PRIVATE_ENCRYPTION_KEY;
-import static uk.gov.ida.saml.core.test.TestCertificateStrings.TEST_RP_PUBLIC_ENCRYPTION_CERT;
 import static uk.gov.ida.verifyserviceprovider.builders.ComplianceToolInitialisationRequestBuilder.aComplianceToolInitialisationRequest;
-import static uk.gov.ida.verifyserviceprovider.dto.LevelOfAssurance.LEVEL_1;
 import static uk.gov.ida.verifyserviceprovider.dto.LevelOfAssurance.LEVEL_2;
 import static uk.gov.ida.verifyserviceprovider.dto.Scenario.SUCCESS_MATCH;
-import static uk.gov.ida.verifyserviceprovider.services.ComplianceToolService.BASIC_SUCCESSFUL_MATCH_WITH_LOA1_ID;
 import static uk.gov.ida.verifyserviceprovider.services.ComplianceToolService.BASIC_SUCCESSFUL_MATCH_WITH_LOA2_ID;
 
 public class SecondaryEncryptionKeyAcceptanceTest {
