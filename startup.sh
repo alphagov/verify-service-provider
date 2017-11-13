0e55245b29c71087ef4d2c719f3ecd88e6062d5e
Rachael Booth <rachael.booth@digital.cabinet-office.gov.uk>
2017-11-13 16:23:46 +0000

    TT-1600 Startup options to allow running as part of a local federation as well as against reference compliance tool
    Adds a config file with environment variable overrides designed for use
    with a verify-local-startup generated local.env file in order to run the
    service provider against a locally running hub.
    Adds a vsp-only env file containing the old defaults to simplify running
    the service provider against compliance tool on reference.
    Adds options to startup.sh to make easy use of these.
    
    Authors: @rachaelbooth

--- startup.sh
+++ startup.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
-# Use the local environment if one exists
-if test -e local.env; then
-    source local.env
-else
-    printf "$(tput setaf 1)No local environment found. Use verify-local-startup or openssl to generate a local.env file\n$(tput sgr0)"
-fi
+CONFIG_FILE=./verify-service-provider.yml
+
+case $1 in
+    'local-fed')
+        CONFIG_FILE=./local-running/local-config.yml
+        if test -e local.env; then
+            source local.env
+        else
+            printf "$(tput setaf 1)No local environment found. Use verify-local-startup or openssl to generate a local.env file\n$(tput sgr0)"
+        fi
+        ;;
+    'vsp-only')
+        source ./local-running/local-vsp-only.env
+        ;;
+esac
 
 cd "$(dirname "$0")"
 
 ./gradlew installDist
 
-./build/install/verify-service-provider/bin/verify-service-provider server ./verify-service-provider.yml
+./build/install/verify-service-provider/bin/verify-service-provider server $CONFIG_FILE
