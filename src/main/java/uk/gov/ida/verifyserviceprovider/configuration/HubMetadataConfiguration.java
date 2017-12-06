b3e14e5b01cea64747e2633b9e88598fd695f23c
Keerthana Subramanian <keerthas@thoughtworks.com>
2017-12-06 14:25:42 +0000

    BAU: Changing property "client" to "jerseyClientConfiguration" as it says in Advanced_confiuration.md file
    Authors: @SKeerthana

--- src/main/java/uk/gov/ida/verifyserviceprovider/configuration/HubMetadataConfiguration.java
+++ src/main/java/uk/gov/ida/verifyserviceprovider/configuration/HubMetadataConfiguration.java
@@ -3,25 +3,17 @@ package uk.gov.ida.verifyserviceprovider.configuration;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.base.Throwables;
 import io.dropwizard.client.JerseyClientConfiguration;
-import io.dropwizard.servlets.assets.ResourceNotFoundException;
-import uk.gov.ida.saml.metadata.EncodedTrustStoreConfiguration;
-import uk.gov.ida.saml.metadata.KeyStoreLoader;
 import uk.gov.ida.saml.metadata.MetadataConfiguration;
 import uk.gov.ida.saml.metadata.TrustStoreConfiguration;
 import uk.gov.ida.saml.metadata.exception.EmptyTrustStoreException;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.net.URI;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 
 import static java.util.Optional.ofNullable;
 import static uk.gov.ida.verifyserviceprovider.configuration.ConfigurationConstants.HUB_JERSEY_CLIENT_NAME;
-import static uk.gov.ida.verifyserviceprovider.configuration.ConfigurationConstants.PRODUCTION_VERIFY_TRUSTSTORE_NAME;
-import static uk.gov.ida.verifyserviceprovider.configuration.ConfigurationConstants.TEST_VERIFY_TRUSTSTORE_NAME;
 
 public class HubMetadataConfiguration extends MetadataConfiguration {
 
@@ -34,11 +26,11 @@ public class HubMetadataConfiguration extends MetadataConfiguration {
             @JsonProperty("minRefreshDelay") Long minRefreshDelay,
             @JsonProperty("maxRefreshDelay") Long maxRefreshDelay,
             @JsonProperty("expectedEntityId") String expectedEntityId,
-            @JsonProperty("client") JerseyClientConfiguration client,
+            @JsonProperty("jerseyClientConfiguration") JerseyClientConfiguration jerseyClientConfiguration,
             @JsonProperty("jerseyClientName") String jerseyClientName,
             @JsonProperty("hubFederationId") String hubFederationId,
             @JsonProperty("trustStore") TrustStoreConfiguration trustStoreConfiguration) {
-        super(uri, minRefreshDelay, maxRefreshDelay, expectedEntityId, client,
+        super(uri, minRefreshDelay, maxRefreshDelay, expectedEntityId, jerseyClientConfiguration,
                 ofNullable(jerseyClientName).orElse(HUB_JERSEY_CLIENT_NAME), hubFederationId);
         this.trustStoreConfiguration = trustStoreConfiguration;
     }
