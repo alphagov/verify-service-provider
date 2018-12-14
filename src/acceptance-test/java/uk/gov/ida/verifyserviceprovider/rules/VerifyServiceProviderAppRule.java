982f0497daf23087b06abf3d8cbc70cb12ef6fdd
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2018-12-14 12:17:56 +0000

    HstMcl3: refactoring non matching acceptance test with MSA metadata dependencies
    Making MSA data optional in the VSP configuration optional by making it nullable
    in the case of non matching https://trello.com/c/NHstMcl3/242-make-msa-metadata-optional
    Acceptance criteria is when all non-matching test that contain MSA configuration can
    be made to pass without requiring MSA configuration data. Steps involved requires
    
    - Removing multiple classes for non matching acceptance test
    - This ensures there is only one place requiring MSA configuration data

--- src/acceptance-test/java/uk/gov/ida/verifyserviceprovider/rules/VerifyServiceProviderAppRule.java
+++ src/acceptance-test/java/uk/gov/ida/verifyserviceprovider/rules/VerifyServiceProviderAppRule.java
@@ -6,12 +6,27 @@ import io.dropwizard.testing.junit.DropwizardAppRule;
 import uk.gov.ida.saml.core.IdaSamlBootstrap;
 import uk.gov.ida.verifyserviceprovider.VerifyServiceProviderApplication;
 import uk.gov.ida.verifyserviceprovider.configuration.VerifyServiceProviderConfiguration;
-
 import static uk.gov.ida.saml.core.test.TestCertificateStrings.TEST_RP_PRIVATE_ENCRYPTION_KEY;
 import static uk.gov.ida.saml.core.test.TestCertificateStrings.TEST_RP_PRIVATE_SIGNING_KEY;
 
 public class VerifyServiceProviderAppRule extends DropwizardAppRule<VerifyServiceProviderConfiguration> {
 
+
+    public VerifyServiceProviderAppRule(String secondaryEncryptionKey, String serviceEntityIdOverride) {
+        super(
+            VerifyServiceProviderApplication.class,
+            "verify-service-provider.yml",
+            ConfigOverride.config("serviceEntityIds", serviceEntityIdOverride),
+            ConfigOverride.config("hashingEntityId", "some-hashing-entity-id"),
+            ConfigOverride.config("server.connector.port", String.valueOf(0)),
+            ConfigOverride.config("logging.loggers.uk\\.gov", "DEBUG"),
+            ConfigOverride.config("samlSigningKey", TEST_RP_PRIVATE_SIGNING_KEY),
+            ConfigOverride.config("verifyHubConfiguration.environment", "COMPLIANCE_TOOL"),
+            ConfigOverride.config("samlPrimaryEncryptionKey", TEST_RP_PRIVATE_ENCRYPTION_KEY),
+            ConfigOverride.config("samlSecondaryEncryptionKey", secondaryEncryptionKey),
+            ConfigOverride.config("msaMetadata.uri", "http://some-msa-uri")
+        );
+    }
     public VerifyServiceProviderAppRule(MockMsaServer msaServer, String secondaryEncryptionKey, String serviceEntityIdOverride) {
         super(
             VerifyServiceProviderApplication.class,
@@ -33,11 +48,15 @@ public class VerifyServiceProviderAppRule extends DropwizardAppRule<VerifyServic
         );
     }
 
+    public VerifyServiceProviderAppRule() {
+        this(TEST_RP_PRIVATE_ENCRYPTION_KEY, "http://verify-service-provider");
+    }
+
     public VerifyServiceProviderAppRule(MockMsaServer msaServer) {
-        this(msaServer, TEST_RP_PRIVATE_ENCRYPTION_KEY, "http://verify-service-provider");
+        this(msaServer,TEST_RP_PRIVATE_ENCRYPTION_KEY, "http://verify-service-provider");
     }
 
     public VerifyServiceProviderAppRule(MockMsaServer msaServer, String serviceEntityIdOverride) {
-        this(msaServer, TEST_RP_PRIVATE_ENCRYPTION_KEY, serviceEntityIdOverride);
+        this(msaServer,TEST_RP_PRIVATE_ENCRYPTION_KEY, serviceEntityIdOverride);
     }
 }
