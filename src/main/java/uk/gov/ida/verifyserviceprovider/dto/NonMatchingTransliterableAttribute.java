512bd070ef419ab95b4e40ac59888ebba4e5bad6
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2019-02-27 09:06:14 +0000

    9ZNOyJJW: don't serialize null `nonLatinScriptValue`

--- src/main/java/uk/gov/ida/verifyserviceprovider/dto/NonMatchingTransliterableAttribute.java
+++ src/main/java/uk/gov/ida/verifyserviceprovider/dto/NonMatchingTransliterableAttribute.java
@@ -1,12 +1,13 @@
 package uk.gov.ida.verifyserviceprovider.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDate;
 
 public class NonMatchingTransliterableAttribute extends NonMatchingVerifiableAttribute<String> {
 
-    @JsonProperty("nonLatinScriptValue")
+    @JsonProperty("nonLatinScriptValue") @JsonInclude(value = JsonInclude.Include.NON_NULL)
     private final String nonLatinScriptValue;
 
     public NonMatchingTransliterableAttribute(String value,
