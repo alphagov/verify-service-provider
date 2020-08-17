3d04d11b7e647979b9546b49c52dd6b6aff9aa2c
Lewis Westbury <lewis.westbury@digital.cabinet-office.gov.uk>
2020-08-17 19:06:27 +0100

    Alter tests to include failure modes for translation requests that do not include requestId.

--- src/acceptance-test/java/uk/gov/ida/verifyserviceprovider/SuccessMatchAcceptanceTest.java
+++ src/acceptance-test/java/uk/gov/ida/verifyserviceprovider/SuccessMatchAcceptanceTest.java
@@ -7,15 +7,20 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
+import uk.gov.ida.saml.deserializers.StringToOpenSamlObjectTransformer;
+import uk.gov.ida.saml.serializers.XmlObjectToBase64EncodedStringTransformer;
 import uk.gov.ida.verifyserviceprovider.dto.RequestResponseBody;
 import uk.gov.ida.verifyserviceprovider.dto.TranslatedMatchingResponseBody;
+import uk.gov.ida.verifyserviceprovider.factories.saml.ResponseFactory;
 import uk.gov.ida.verifyserviceprovider.rules.VerifyServiceProviderAppRule;
 import uk.gov.ida.verifyserviceprovider.services.ComplianceToolService;
 import uk.gov.ida.verifyserviceprovider.services.GenerateRequestService;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.core.Response;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import static javax.ws.rs.client.Entity.json;
 import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
@@ -169,5 +174,148 @@ public class SuccessMatchAcceptanceTest {
         ErrorMessage errorMessage = response.readEntity(ErrorMessage.class);
         assertThat(errorMessage.getCode()).isEqualTo(BAD_REQUEST.getStatusCode());
         assertThat(errorMessage.getMessage()).isEqualTo("Provided entityId: invalidEntityId is not listed in config");
+    }
+
+    @Test
+    public void shouldReturnAnErrorWhenNullRequestIdIsProvided() {
+        RequestResponseBody requestResponseBody = generateRequestService.generateAuthnRequest(application.getLocalPort());
+
+        Map<String, String> translateResponseRequestData = new HashMap<>(); // not a lot of map implementations allow nulls
+        translateResponseRequestData.put(
+            "samlResponse", complianceTool.createResponseFor(requestResponseBody.getSamlRequest(), BASIC_SUCCESSFUL_MATCH_WITH_LOA2_ID));
+        translateResponseRequestData.put("requestId", null);
+        translateResponseRequestData.put("levelOfAssurance", LEVEL_2.name());
+
+        Response response = client
+            .target(String.format("http://localhost:%d/translate-response", application.getLocalPort()))
+            .request()
+            .buildPost(json(translateResponseRequestData))
+            .invoke();
+
+        assertThat(response.getStatus()).isEqualTo(422);
+        ErrorMessage errorMessage = response.readEntity(ErrorMessage.class);
+        assertThat(errorMessage.getCode()).isEqualTo(422);
+        assertThat(errorMessage.getMessage()).isEqualTo("requestId may not be null");
+    }
+
+    @Test
+    public void shouldReturnAnErrorForTheNullRequestIdProvidedRegardlessOfIncorrectSaml() {
+        RequestResponseBody requestResponseBody = generateRequestService.generateAuthnRequest(application.getLocalPort());
+
+        Map<String, String> translateResponseRequestData = new HashMap<>(); // not a lot of map implementations allow nulls
+        translateResponseRequestData.put("samlResponse", "not a SAML response");
+        translateResponseRequestData.put("requestId", null);
+        translateResponseRequestData.put("levelOfAssurance", LEVEL_2.name());
+
+        Response response = client
+            .target(String.format("http://localhost:%d/translate-response", application.getLocalPort()))
+            .request()
+            .buildPost(json(translateResponseRequestData))
+            .invoke();
+
+        assertThat(response.getStatus()).isEqualTo(422);
+        ErrorMessage errorMessage = response.readEntity(ErrorMessage.class);
+        assertThat(errorMessage.getCode()).isEqualTo(422);
+        assertThat(errorMessage.getMessage()).isEqualTo("requestId may not be null");
+    }
+
+    @Test
+    public void shouldReturnAnErrorForAlteredSamlResponses() {
+        RequestResponseBody requestResponseBody = generateRequestService.generateAuthnRequest(application.getLocalPort());
+
+        String saml = complianceTool.createResponseFor(requestResponseBody.getSamlRequest(), BASIC_SUCCESSFUL_MATCH_WITH_LOA2_ID);
+
+        StringToOpenSamlObjectTransformer<org.opensaml.saml.saml2.core.Response> deserializer = ResponseFactory.createStringToResponseTransformer();
+        org.opensaml.saml.saml2.core.Response authenticationResponse = deserializer.apply(saml);
+        authenticationResponse.setInResponseTo(null);
+
+        XmlObjectToBase64EncodedStringTransformer<org.opensaml.saml.saml2.core.Response> reserializer = new XmlObjectToBase64EncodedStringTransformer<>();
+        String alteredEncodedSaml = reserializer.apply(authenticationResponse);
+
+        Map<String, String> translateResponseRequestData = new HashMap<>();
+        translateResponseRequestData.put("samlResponse", alteredEncodedSaml);
+        translateResponseRequestData.put("requestId", requestResponseBody.getRequestId());
+        translateResponseRequestData.put("levelOfAssurance", LEVEL_2.name());
+
+        Response response = client
+            .target(String.format("http://localhost:%d/translate-response", application.getLocalPort()))
+            .request()
+            .buildPost(json(translateResponseRequestData))
+            .invoke();
+
+        assertThat(response.getStatus()).isEqualTo(BAD_REQUEST.getStatusCode());
+        ErrorMessage errorMessage = response.readEntity(ErrorMessage.class);
+        assertThat(errorMessage.getCode()).isEqualTo(BAD_REQUEST.getStatusCode());
+        assertThat(errorMessage.getMessage()).isEqualTo("SAML Validation Specification: Message signature is not signed\n" +
+            "DocumentReference{documentName='Hub Service Profile 1.1a', documentSection=''}");
+    }
+
+    @Test
+    public void shouldReturnAnErrorForNonSamlResponses() {
+        RequestResponseBody requestResponseBody = generateRequestService.generateAuthnRequest(application.getLocalPort());
+
+        Map<String, String> translateResponseRequestData = new HashMap<>();
+        translateResponseRequestData.put("samlResponse", "not a SAML response");
+        translateResponseRequestData.put("requestId", requestResponseBody.getRequestId());
+        translateResponseRequestData.put("levelOfAssurance", LEVEL_2.name());
+
+        Response response = client
+            .target(String.format("http://localhost:%d/translate-response", application.getLocalPort()))
+            .request()
+            .buildPost(json(translateResponseRequestData))
+            .invoke();
+
+        assertThat(response.getStatus()).isEqualTo(BAD_REQUEST.getStatusCode());
+        ErrorMessage errorMessage = response.readEntity(ErrorMessage.class);
+        assertThat(errorMessage.getCode()).isEqualTo(BAD_REQUEST.getStatusCode());
+        assertThat(errorMessage.getMessage()).startsWith("SAML Validation Specification: Unable to deserialize string into OpenSaml object");
+    }
+
+    @Test
+    public void shouldReturnAnErrorWhenAnEmptyRequestIdIsProvided() {
+        RequestResponseBody requestResponseBody = generateRequestService.generateAuthnRequest(application.getLocalPort());
+
+        Map<String, String> translateResponseRequestData = new HashMap<>(); // not a lot of map implementations allow nulls
+        translateResponseRequestData.put(
+            "samlResponse", complianceTool.createResponseFor(requestResponseBody.getSamlRequest(), BASIC_SUCCESSFUL_MATCH_WITH_LOA2_ID));
+        translateResponseRequestData.put("requestId", "");
+        translateResponseRequestData.put("levelOfAssurance", LEVEL_2.name());
+
+        Response response = client
+            .target(String.format("http://localhost:%d/translate-response", application.getLocalPort()))
+            .request()
+            .buildPost(json(translateResponseRequestData))
+            .invoke();
+
+        assertThat(response.getStatus()).isEqualTo(BAD_REQUEST.getStatusCode());
+        ErrorMessage errorMessage = response.readEntity(ErrorMessage.class);
+        assertThat(errorMessage.getCode()).isEqualTo(BAD_REQUEST.getStatusCode());
+        assertThat(errorMessage.getMessage()).isEqualTo(
+            String.format("Expected InResponseTo to be %s, but was %s", "", requestResponseBody.getRequestId()));
+    }
+
+    @Test
+    public void shouldReturnAnErrorWhenAnIncorrectRequestIdIsProvided() {
+        RequestResponseBody requestResponseBody = generateRequestService.generateAuthnRequest(application.getLocalPort());
+
+        String falseRequestId = "_" + UUID.randomUUID().toString();
+
+        Map<String, String> translateResponseRequestData = new HashMap<>(); // not a lot of map implementations allow nulls
+        translateResponseRequestData.put(
+            "samlResponse", complianceTool.createResponseFor(requestResponseBody.getSamlRequest(), BASIC_SUCCESSFUL_MATCH_WITH_LOA2_ID));
+        translateResponseRequestData.put("requestId", falseRequestId);
+        translateResponseRequestData.put("levelOfAssurance", LEVEL_2.name());
+
+        Response response = client
+            .target(String.format("http://localhost:%d/translate-response", application.getLocalPort()))
+            .request()
+            .buildPost(json(translateResponseRequestData))
+            .invoke();
+
+        assertThat(response.getStatus()).isEqualTo(BAD_REQUEST.getStatusCode());
+        ErrorMessage errorMessage = response.readEntity(ErrorMessage.class);
+        assertThat(errorMessage.getCode()).isEqualTo(BAD_REQUEST.getStatusCode());
+        assertThat(errorMessage.getMessage()).isEqualTo(
+            String.format("Expected InResponseTo to be %s, but was %s", falseRequestId, requestResponseBody.getRequestId()));
     }
 }
